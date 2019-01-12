118132e0b32234e226466bb670b89f83a3b81093
zim <zim@zim>
2019-01-12 16:42:03 +0100

    DebugPerspectiveCloserOnAllTerminated only acts for debug launches not runs

--- AutoCloseDebugPerspectivePlugin/src/org/eclipseplugins/autoclosedebugperspective/DebugPerspectiveCloserOnAllTerminated.java
+++ AutoCloseDebugPerspectivePlugin/src/org/eclipseplugins/autoclosedebugperspective/DebugPerspectiveCloserOnAllTerminated.java
@@ -11,6 +11,7 @@ import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.debug.core.ILaunchesListener2;
 import org.eclipse.ui.IPerspectiveDescriptor;
 import org.eclipseplugins.autoclosedebugperspective.util.IPerspectiveUtil;
+import org.eclipseplugins.autoclosedebugperspective.util.LaunchUtil;
 
 public class DebugPerspectiveCloserOnAllTerminated implements ILaunchesListener2 {
 
@@ -29,17 +30,29 @@ public class DebugPerspectiveCloserOnAllTerminated implements ILaunchesListener2
 	@Override
 	public void launchesAdded(ILaunch[] launchesAdded) {
 		try {
-			ILaunch[] launches = launchManager.getLaunches();
-			long nonTerminatedLaunchesCount = Arrays.stream(launches).filter(l -> !l.isTerminated()).count();
-			long nonTerminatedAddedLaunchesCount = Arrays.stream(launchesAdded).filter(l -> !l.isTerminated()).count();
-			if (nonTerminatedLaunchesCount == nonTerminatedAddedLaunchesCount) {
+			long addedActiveDebugLaunchesCount = 
+					Arrays.stream(launchesAdded)
+					.filter(l -> isNonTerminatedDebugLaunch(l))
+					.count();
+
+			if (addedActiveDebugLaunchesCount <= 0)
+				return;
+			
+			long totalActiveDebugLaunchesCount = 
+					Arrays.stream(launchManager.getLaunches())
+					.filter(l -> isNonTerminatedDebugLaunch(l))
+					.count();
+			
+			// launchesAdded have been already added and thus are returned in launchManager.getLaunches().
+			// Therefore for the first launch the count of added launches is equal to the total count returned by getLaunches() (filtered by debug and not terminated).
+			if (totalActiveDebugLaunchesCount == addedActiveDebugLaunchesCount) {
 				IPerspectiveDescriptor p = perspectiveUtil.getCurrentPerspective();
 				perspectiveOnFirstLaunch = Optional.of(p);
 				log.log(new Status(IStatus.OK, Activator.PLUGIN_ID, "first launch from perspective" + p));
 			} else {
 				log.log(new Status(IStatus.INFO, Activator.PLUGIN_ID,
-						"not first launch: nonTerminatedLaunchesCount:" + nonTerminatedLaunchesCount
-								+ ", nonTerminatedAddedLaunchesCount: " + nonTerminatedAddedLaunchesCount));
+						"not first launch: totalActiveDebugLaunchesCount:" + totalActiveDebugLaunchesCount
+								+ ", addedActiveDebugLaunchesCount: " + addedActiveDebugLaunchesCount));
 			}
 		} catch (Exception e) {
 			log.log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, "", e));
@@ -49,8 +62,17 @@ public class DebugPerspectiveCloserOnAllTerminated implements ILaunchesListener2
 	@Override
 	public void launchesTerminated(ILaunch[] launchesTerminated) {
 		try {
+			boolean hasAnyDebugLaunchTerminated = Arrays.stream(launchesTerminated).anyMatch(LaunchUtil::isDebugLaunch);
+			if (!hasAnyDebugLaunchTerminated)
+				return;
+			
 			ILaunch[] launches = launchManager.getLaunches();
-			if (Arrays.stream(launches).allMatch(l -> l.isTerminated())) {
+			boolean haveAllDebugLaunchesTerminated = 
+					Arrays.stream(launches)
+					.filter(LaunchUtil::isDebugLaunch)
+					.allMatch(ILaunch::isTerminated);
+			
+			if (haveAllDebugLaunchesTerminated) {
 				perspectiveOnFirstLaunch.ifPresent(p -> {
 					if (!perspectiveUtil.isDebugPerspective(p)) {
 						log.log(new Status(IStatus.OK, Activator.PLUGIN_ID, "setting perspective " + p));
@@ -58,10 +80,16 @@ public class DebugPerspectiveCloserOnAllTerminated implements ILaunchesListener2
 					}
 					perspectiveOnFirstLaunch = Optional.empty();
 				});
+			} else {
+				log.log(new Status(IStatus.INFO, Activator.PLUGIN_ID, "some debug launch(es) have terminated, but not all"));
 			}
 		} catch (Exception e) {
 			log.log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, "", e));
 		}
+	}
+	
+	private boolean isNonTerminatedDebugLaunch(ILaunch l) {
+		return LaunchUtil.isDebugLaunch(l) && !l.isTerminated();
 	}
 
 	@Override
